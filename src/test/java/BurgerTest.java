67b77a3e853d046c17cb2f0089e279d83de7f18e
Sergey Vasilyev <s.s.vasilev@gmail.com>
2023-02-07 00:54:00 +0300

    Закончил покрывать классы unit-тестами

--- src/test/java/BurgerTest.java
+++ src/test/java/BurgerTest.java
@@ -1,57 +1,81 @@
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
 import praktikum.Bun;
+import praktikum.Burger;
 import praktikum.Ingredient;
-
+import praktikum.IngredientType;
 import java.util.List;
 
-import static org.hamcrest.CoreMatchers.notNullValue;
 
-@RunWith(Parameterized.class)
+@RunWith(MockitoJUnitRunner.class)
 public class BurgerTest {
 
-
-        private final Bun bun;
-        private final List<Ingredient> ingredients;
-
-        public BurgerTest(Bun bunSet, List<Ingredient> ingredientsSet) {
-            this.bun = bunSet;
-            this.ingredients = ingredientsSet;
-
-        }
+    @Mock
+    Bun bun;
+    @Mock
+    Ingredient ingredient;
+    @Mock
+    Ingredient secondIngredient;
 
 
-        // Тестовые данные
-        @Parameterized.Parameters
-        public static Object[][] getDataOrder() {
-            return new Object[][]{
-                    {"Никита", "Комбаров", "Комсомольская ул., д. 6 кв.152", "Черкизовская", "89888678901", "1", "30.11.2022", "Прикрепить бантик на руль", "[\"black\"]"},
-                    {"Пётр", "Петрович", "Чкалова ул., д. 15 кв.55", "Китай-город", "89881235897", "2", "22.08.2023", "Оставить у двери", "[\"grey\"]"},
-                    {"Леонид", "Ксенофонтов", "Вокзальная ул., д. 19 кв.170", "Речной вокзал", "89882836886", "3", "27.05.2024", " ", "[\"black\",\"grey\"]"},
-                    {"Александр", "Пушкин", "Мира ул., д. 20 кв.35", "Комсомольская", "89889783152", "4", "13.05.2024", "Ничего не делать", "[]"},
+    @Test
+    public void setBunsReturnBun(){
+        Burger burger = new Burger();
+        burger.setBuns(bun);
+        Assert.assertEquals(bun,burger.bun);
+    }
 
-            };
-        }
+    @Test
+    public void addIngredientAddListIngredient(){
+        Burger burger = new Burger();
+        burger.addIngredient(ingredient);
+        Assert.assertEquals(List.of(ingredient),burger.ingredients);
+    }
 
-//        @Test
-//        @DisplayName("Создание заказа (Параметризованный)") // имя теста
-//        @Description("Параметры учитывают все варианты из задания") // описание теста
-//        public void restCreateOrder() {
-//            //Собираем из тестовых данных JSON, который послужит телом для запроса.
-//            Order order = new Order(firstNameSet,lastNameSet,addressSet,metroStationSet,phoneSet,rentTimeSet,deliveryDateSet,commentSet,colorSet);
-//
-//            //Отправляем запрос
-//            Response response = OrderApi.createOrder(order);
-//            //Проверяем, что в ответе содержится track и оно не пустое.
-//            response
-//                    .then().assertThat().body("track", notNullValue())
-//                    .and()
-//                    .statusCode(SC_CREATED); //Проверяем, что код ответа = 201
-//
-//        }
+    @Test
+    public void removeIngredientDeleteIngredient(){
+        Burger burger = new Burger();
+        burger.addIngredient(ingredient);
+        burger.removeIngredient(0);
+        Assert.assertEquals(List.of(),burger.ingredients);
+    }
 
+    @Test
+    public void moveIngredientMoveIngredient(){
+        Burger burger = new Burger();
+        burger.addIngredient(ingredient);
+        burger.addIngredient(secondIngredient);
+        burger.moveIngredient(0,1);
+        Assert.assertEquals(List.of(secondIngredient,ingredient),burger.ingredients);
+    }
 
+    @Test
+    public void getPriceReturnPrice(){
+        Burger burger = new Burger();
+        Mockito.when(bun.getPrice()).thenReturn(988F);
+        burger.setBuns(bun);
+        Mockito.when(ingredient.getPrice()).thenReturn(90F);
+        burger.addIngredient(ingredient);
+        Assert.assertEquals(2066.0,burger.getPrice(),0.1);
+    }
 
+    @Test
+    public void  getReceiptReturnReceipt(){
+        Burger burger = new Burger();
+        Mockito.when(bun.getName()).thenReturn("Флюоресцентная булка R2-D3");
+        Mockito.when(bun.getPrice()).thenReturn(988F);
+        Mockito.when(ingredient.getName()).thenReturn("Соус Spicy-X");
+        Mockito.when(ingredient.getPrice()).thenReturn(90F);
+        Mockito.when(ingredient.getType()).thenReturn(IngredientType.SAUCE);
+        burger.setBuns(bun);
+        burger.addIngredient(ingredient);
+        Assert.assertEquals(
+                "(==== Флюоресцентная булка R2-D3 ====)\r\n= sauce Соус Spicy-X =\r\n(==== Флюоресцентная булка R2-D3 ====)\r\n\r\nPrice: 2066,000000\r\n"
+,burger.getReceipt());
+    }
 
 }
