8b86a258d59e0374f2e6ef2736b58ab396ff2bce
Sergey Vasilyev <s.s.vasilev@gmail.com>
2023-02-07 18:59:40 +0300

    Провёл оценку покрытия и прикрепил отчёт Jacoco

--- src/test/java/IngredientTest.java
+++ src/test/java/IngredientTest.java
@@ -1,35 +1,34 @@
 import org.junit.Assert;
 import org.junit.Test;
+import org.mockito.Mock;
 import praktikum.Ingredient;
-import static praktikum.IngredientType.*;
+import praktikum.IngredientType;
 
 
 public class IngredientTest {
 
 
+    @Mock
+    IngredientType ingredientType;
+
+
 
     @Test
     public void getNameReturnName() {
-        Ingredient ingredient = new Ingredient(SAUCE,"Соус Spicy-X",90);
+        Ingredient ingredient = new Ingredient(ingredientType,"Соус Spicy-X",90);
         Assert.assertEquals("Соус Spicy-X", ingredient.getName());
     }
 
     @Test
     public void getPriceReturnPrice() {
-        Ingredient ingredient = new Ingredient(SAUCE,"Соус Spicy-X",90);
+        Ingredient ingredient = new Ingredient(ingredientType,"Соус Spicy-X",90);
         Assert.assertEquals(90, ingredient.getPrice(),0.1);
     }
 
     @Test
-    public void getTypeSAUCEReturnTypeSAUCE() {
-        Ingredient ingredient = new Ingredient(SAUCE,"Соус Spicy-X",90);
-        Assert.assertEquals(SAUCE, ingredient.getType());
-    }
-
-    @Test
-    public void getTypeFILLINGReturnTypeFILLING() {
-        Ingredient ingredient = new Ingredient(FILLING,"Мясо бессмертных моллюсков Protostomia",1337);
-        Assert.assertEquals(FILLING, ingredient.getType());
+    public void getTypeReturningredientType() {
+        Ingredient ingredient = new Ingredient(ingredientType,"Соус Spicy-X",90);
+        Assert.assertEquals(ingredientType, ingredient.getType());
     }
 
 
